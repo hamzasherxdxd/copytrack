3cf4eeaa09f6b4e7421073f1a6a975d587f89ab3
hamzasherxdxd <hamzasher223@gmail.com>
2023-01-20 14:06:32 +0500

    Front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect, createContext } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/theme";
+import Routes from "./Routes";
+import Nav from "./components/Nav";
+
+export const UserContext = createContext({});
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [userSession, setUserSession] = useState(true);
+
+  useEffect(() => {
+    const fetchUserAuth = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch("/api/isAuth");
+        if (!res.ok) return setLoading(false);
+        setUserSession(await res.json());
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.error("There was an error fetching", error);
+        return;
+      }
+    };
+    fetchUserAuth();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={userSession}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Nav />
+        <br />
+        {loading ? <>loading...</> : <Routes />}
+      </ThemeProvider>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,146 @@
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import axios from "axios";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+const Home = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [projects, setProjects] = useState([]);
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [emailNotification, setEmailNotification] = useState(false);
+  const [response, setResponse] = useState("");
+  const fetchProjects = (e) => {
+    axios.get("/api/project").then((response) => {
+      setProjects(response.data.projects);
+      console.log(projects);
+    });
+  };
+  useEffect(() => {
+    fetchProjects();
+  }, [response]);
+
+  const handleProjectNameChange = (e) => {
+    setProjectName(e.target.value);
+  };
+  const handleProjectDescriptionChange = (e) => {
+    setProjectDescription(e.target.value);
+  };
+  const handleCreateProjectFormOpen = (e) => {
+    setDialogOpen(true);
+  };
+  const handleCreateProjectFormClose = (e) => {
+    setDialogOpen(false);
+  };
+  const handleEmailNotificationChange = (e) => {
+    setEmailNotification(e.target.checked);
+  };
+  const handleCreateProject = (e) => {
+    const options = {
+      url: "http://localhost:3000/api/project",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      withCredentials: true,
+      data: {
+        project_name: projectName,
+        description: projectDescription,
+        email_notification: emailNotification,
+      },
+    };
+    axios(options).then((res) => {
+      console.log(res.status);
+      setResponse(res.status);
+      setDialogOpen(false);
+    });
+  };
+
+  return (
+    <Grid container spacing={2}>
+      {projects.map((project) => (
+        <Grid item xs={2}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {project.project_name}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {project.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant="outlined" size="small">
+                Dashboard
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+      <Grid item xs={2}>
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={(e) => handleCreateProjectFormOpen(e)}
+        >
+          Add New Project
+        </Button>
+      </Grid>
+      <Dialog open={dialogOpen} onClose={handleCreateProjectFormClose}>
+        <DialogTitle>Add Project</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Project Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => handleProjectNameChange(e)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Project Description"
+            type="textarea"
+            fullWidth
+            variant="standard"
+            onChange={(e) => handleProjectDescriptionChange(e)}
+          />
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch onChange={(e) => handleEmailNotificationChange(e)} />
+              }
+              label="Email Notifications"
+            />
+          </FormGroup>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCreateProjectFormClose}>Cancel</Button>
+          <Button onClick={handleCreateProject}>Create Project</Button>
+        </DialogActions>
+      </Dialog>
+      {/* <Card variant="outlined">{card}</Card> */}
+    </Grid>
+  );
+};
+
+export default Home;
